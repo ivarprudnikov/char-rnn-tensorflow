c046423f4107bc92b37edc8a9056ffb6a7c50db6
ivarprudnikov <ivar.prudnikov@gmail.com>
2018-10-27 09:03:40 +0100

    Serve basic front facing website from server

--- server/server.js
+++ server/server.js
@@ -7,6 +7,14 @@ const os = require('os')
 const fs = require('fs')
 const PORT = 8080
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
+app.get('/', function (req, res) {
+    res.render('index')
+})
+
 app.post('/train', function (req, res) {
 
     const id = uuidv1()
