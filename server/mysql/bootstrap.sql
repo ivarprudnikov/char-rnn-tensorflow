857073e09d9284b4b55f67bac22fdb76c44dd8d9
Don Smith <dcsmith@gmail.com>
2019-01-19 19:18:49 -0800

    update default schema with new field

--- server/mysql/bootstrap.sql
+++ server/mysql/bootstrap.sql
@@ -1,5 +1,5 @@
-insert into model set id='01-abcd', name='01', train_params='{}', has_data=0, is_in_progress=0;
-insert into model set id='02-abcd', name='02', train_params='{}', has_data=0, is_in_progress=0;
+insert into model set id='01-abcd', name='01', train_params='{}', sample_params='{}' has_data=0, is_in_progress=0;
+insert into model set id='02-abcd', name='02', train_params='{}', sample_params='{}' has_data=0, is_in_progress=0;
 
 -- add some log output to model
 insert into model_log set model_id='01-abcd', position=1, chunk='exec script';
