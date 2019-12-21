d7b15f1c04d19cd2fafcb990135fde7722be5a59
ivarprudnikov <ivar.prudnikov@gmail.com>
2019-12-21 22:27:26 +0000

    use absl.flags and absl.app instead of same ones in tf.compat.v1

--- generator_v2/train.py
+++ generator_v2/train.py
@@ -1,6 +1,7 @@
-import tensorflow as tf
-from read_utils import TextConverter, batch_generator
-from model import CharRNN
+from absl import app
+from absl import flags
+from .read_utils import TextConverter, batch_generator
+from .model import CharRNN
 import os
 import codecs
 import re
@@ -8,23 +9,21 @@ import logging
 
 logging.getLogger('tensorflow').setLevel(logging.ERROR)
 
-FLAGS = tf.flags.FLAGS
-
-tf.flags.DEFINE_string('name', 'default', 'name of the model')
-tf.flags.DEFINE_integer('num_seqs', 100, 'number of seqs in one batch')
-tf.flags.DEFINE_integer('num_steps', 100, 'length of one seq')
-tf.flags.DEFINE_integer('lstm_size', 128, 'size of hidden state of lstm')
-tf.flags.DEFINE_integer('num_layers', 2, 'number of lstm layers')
-tf.flags.DEFINE_boolean('use_embedding', False, 'whether to use embedding')
-tf.flags.DEFINE_integer('embedding_size', 128, 'size of embedding')
-tf.flags.DEFINE_float('learning_rate', 0.001, 'learning_rate')
-tf.flags.DEFINE_float('train_keep_prob', 0.5, 'dropout rate during training')
-tf.flags.DEFINE_string('input_file', '', 'utf8 encoded text file')
-tf.flags.DEFINE_string('whitelist_file', '', 'utf8 encoded text file')
-tf.flags.DEFINE_integer('max_steps', 100000, 'max steps to train')
-tf.flags.DEFINE_integer('save_every_n', 1000, 'save the model every n steps')
-tf.flags.DEFINE_integer('log_every_n', 10, 'log to the screen every n steps')
-tf.flags.DEFINE_integer('max_vocab', 3500, 'max char number')
+flags.DEFINE_string('name', 'default', 'name of the model')
+flags.DEFINE_integer('num_seqs', 100, 'number of seqs in one batch')
+flags.DEFINE_integer('num_steps', 100, 'length of one seq')
+flags.DEFINE_integer('lstm_size', 128, 'size of hidden state of lstm')
+flags.DEFINE_integer('num_layers', 2, 'number of lstm layers')
+flags.DEFINE_boolean('use_embedding', False, 'whether to use embedding')
+flags.DEFINE_integer('embedding_size', 128, 'size of embedding')
+flags.DEFINE_float('learning_rate', 0.001, 'learning_rate')
+flags.DEFINE_float('train_keep_prob', 0.5, 'dropout rate during training')
+flags.DEFINE_string('input_file', '', 'utf8 encoded text file')
+flags.DEFINE_string('whitelist_file', '', 'utf8 encoded text file')
+flags.DEFINE_integer('max_steps', 100000, 'max steps to train')
+flags.DEFINE_integer('save_every_n', 1000, 'save the model every n steps')
+flags.DEFINE_integer('log_every_n', 10, 'log to the screen every n steps')
+flags.DEFINE_integer('max_vocab', 3500, 'max char number')
 
 
 def remove_non_matching_chars(text, whitelist):
@@ -39,42 +38,43 @@ def remove_non_matching_chars(text, whitelist):
     print("cleaned corpus size " + str(len(text)))
     return text
 
+
 def main(_):
     script_path = os.path.abspath(os.path.dirname(__file__))
-    model_path = os.path.join(script_path, 'model', FLAGS.name)
+    model_path = os.path.join(script_path, 'model', flags.FLAGS.name)
     if os.path.exists(model_path) is False:
         os.makedirs(model_path)
-    with codecs.open(FLAGS.input_file, encoding='utf-8') as f:
+    with codecs.open(flags.FLAGS.input_file, encoding='utf-8') as f:
         text = f.read()
     print("corpus size " + str(len(text)))
 
-    if os.path.exists(FLAGS.whitelist_file):
-        with codecs.open(FLAGS.whitelist_file, encoding='utf-8') as f:
+    if os.path.exists(flags.FLAGS.whitelist_file):
+        with codecs.open(flags.FLAGS.whitelist_file, encoding='utf-8') as f:
             whitelist = f.read()
         text = remove_non_matching_chars(text, whitelist)
 
-    converter = TextConverter(text, FLAGS.max_vocab)
+    converter = TextConverter(text, flags.FLAGS.max_vocab)
     converter.save_to_file(os.path.join(model_path, 'converter.pkl'))
 
     arr = converter.text_to_arr(text)
-    g = batch_generator(arr, FLAGS.num_seqs, FLAGS.num_steps)
+    g = batch_generator(arr, flags.FLAGS.num_seqs, flags.FLAGS.num_steps)
     model = CharRNN(converter.vocab_size,
-                    num_seqs=FLAGS.num_seqs,
-                    num_steps=FLAGS.num_steps,
-                    lstm_size=FLAGS.lstm_size,
-                    num_layers=FLAGS.num_layers,
-                    learning_rate=FLAGS.learning_rate,
-                    train_keep_prob=FLAGS.train_keep_prob,
-                    use_embedding=FLAGS.use_embedding,
-                    embedding_size=FLAGS.embedding_size
+                    num_seqs=flags.FLAGS.num_seqs,
+                    num_steps=flags.FLAGS.num_steps,
+                    lstm_size=flags.FLAGS.lstm_size,
+                    num_layers=flags.FLAGS.num_layers,
+                    learning_rate=flags.FLAGS.learning_rate,
+                    train_keep_prob=flags.FLAGS.train_keep_prob,
+                    use_embedding=flags.FLAGS.use_embedding,
+                    embedding_size=flags.FLAGS.embedding_size
                     )
     model.train(g,
-                FLAGS.max_steps,
+                flags.FLAGS.max_steps,
                 model_path,
-                FLAGS.save_every_n,
-                FLAGS.log_every_n,
+                flags.FLAGS.save_every_n,
+                flags.FLAGS.log_every_n,
                 )
 
 
 if __name__ == '__main__':
-    tf.compat.v1.app.run()
+    app.run(main)
